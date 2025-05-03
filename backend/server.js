47f4569892d2993c4280020b9dfb8d0f0bd27204
Joax48 <114967725+Joax48@users.noreply.github.com>
2025-05-03 08:30:12 -0600

    style: fixing signin styles

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,18 @@
-// backend/server.js
-
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-// Middlewares
+
 app.use(cors());
 app.use(express.json());
 
-// Rutas básicas
 app.get('/', (req, res) => {
-  res.send('Servidor backend funcionando!');
+  res.send('API Centro de Eventos funcionando 🚀');
 });
 
-// Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
