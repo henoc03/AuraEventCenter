38f9073addd4fa2c50928a02ede5a1643aeaec0a
Joax48 <114967725+Joax48@users.noreply.github.com>
2025-05-03 08:32:36 -0600

    feature: mock server

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('API Centro de Eventos funcionando 🚀');
+  res.send('API Centro de Eventos funcionando');
 });
 
 app.listen(PORT, () => {
