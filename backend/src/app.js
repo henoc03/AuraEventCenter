dede2eeeb8e0d83df1c6fc78a0a0abea1d47af45
Henoc Rojas <henoc.rojas@ucr.ac.cr>
2025-05-05 22:35:21 -0600

    Este commit elimina el wallet e instantclient del repositorio y añade las rutas para consultas de salas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const usersRoutes = require('./routes/users');
+const zonesRoutes = require('./routes/zones');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/users', usersRoutes);
+app.use('/zones', zonesRoutes);
 
 app.get('/', (req, res) => {
   res.send('API REST Oracle con tabla USERS ✅');
