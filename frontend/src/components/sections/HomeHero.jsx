d37d25367320d5bd37481a8db601a6fa2d597873
Brayan <brayan.rivera@ucr.ac.cr>
2025-06-01 23:52:36 -0600

    feat: la pantalla de servicios ya trae los servicios de la API correctamente

--- frontend/src/components/sections/HomeHero.jsx
+++ frontend/src/components/sections/HomeHero.jsx
@@ -4,7 +4,9 @@ import Navigation from '../common/Navigation';
 function Hero() {
   return (
    <section className="hero">
-    <Navigation />
+    <div className='home-nav-container'>
+      <Navigation />
+    </div>
     <h1 data-aos="fade-up" data-aos-duration="1500">Centro de Eventos Aura</h1>
     <h2 data-aos="fade-up" data-aos-delay="350" data-aos-duration="5500">Descubrí una experiencia única en un espacio moderno, sostenible y diseñado para todo tipo de eventos. 
       Versatilidad, tecnología y naturaleza en perfecta armonía.</h2>
--- frontend/src/pages/ServicesClient.jsx
+++ frontend/src/pages/ServicesClient.jsx
@@ -4,10 +4,12 @@ import 'aos/dist/aos.css';
 import "../style/services-client.css";
 import heroImage from "../assets/images/services-hero.jpg";
 import Hero from "../components/sections/RoomsServicesHero";
+import Footer from "../components/common/Footer";
 import LoadingPage from "../components/common/LoadingPage";
 
 const DEFAULT_ROUTE = "http://localhost:1522";
 
+// Componente de la pagina de servicios para el cliente
 function ServicesClient() {
   const [services, setServices] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -20,8 +22,10 @@ function ServicesClient() {
     getServices();
   }, []);
 
+  // Función pra traer los servicios de la base de datos
   const getServices = async () => {
     try {
+      // Solicitud de todos los servicios al backend
       const res = await fetch(`${DEFAULT_ROUTE}/services/`, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
@@ -33,6 +37,7 @@ function ServicesClient() {
         return;
       }
 
+      // Almacenar respuesta con los servicios
       const servicesData = await res.json();
       setServices(servicesData);
     } catch (error) {
@@ -57,16 +62,33 @@ function ServicesClient() {
       <main className="services-client-main">
         <h2>Conoce nuestros servicios</h2>
         {services.map((service) => (
-          <div key={service.ADDITIONAL_SERVICE_ID} className="service-container">
-            <img src={service.IMAGE_PATH} alt={service.NAME} />
-            <div className="service-info-container">
+          <div 
+            key={service.ADDITIONAL_SERVICE_ID}   className="service-container">
+            <img 
+              src="https://brownbrotherscatering.com/wp-content/uploads/Exploring-the-Importance-of-Good-Service-in-Catering.jpg"
+              alt={service.NAME}
+              data-aos={service.ADDITIONAL_SERVICE_ID %2 != 0 ? "fade-right" : "fade-left"}
+              data-aos-duration="1500"
+            />
+            <div
+              className="service-info-container"
+              data-aos={service.ADDITIONAL_SERVICE_ID %2 != 0 ? "fade-left" : "fade-right"}
+              data-aos-duration="1500"
+            >
               <h3>{service.NAME}</h3>
               <p>{service.DESCRIPTION}</p>
-              <p>{`Precio: ${service.PRICE}`}</p>
+              <p>{`Precio: ₡${service.PRICE.toLocaleString('es-CR')}`}</p>
+              {service.NAME.toLowerCase().includes("catering") && (
+                <a href="/menus" className="service-menu-link">
+                  <button type='button'>Ver menús</button>
+                </a>
+              )}
             </div>
           </div>
         ))}
       </main>
+
+      <Footer />
     </div>
   );
 }
