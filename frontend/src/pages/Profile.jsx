8b9c9805f0eb91c5fc08cfb44ad144e1383fce24
Brayan <brayan.rivera@ucr.ac.cr>
2025-06-08 10:45:18 -0600

    fix: Estilos de la página de perfil corregidos

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,13 +1,14 @@
 import React, {useEffect, useState } from "react";
 import { useForm} from 'react-hook-form';
 import {jwtDecode} from 'jwt-decode';
+import { useNavigate } from "react-router-dom";
 import Header from "../components/common/Header";
 import ProfilePhoto from "/default-image.jpg"
 import SideNav from "../components/common/SideNav.jsx"
 import AlertMessage from "../components/common/AlertMessage.jsx"
+import LoadingPage from "../components/common/LoadingPage.jsx";
 import '../style/auth.css';
 import '../style/profile.css';
-import { useNavigate } from "react-router-dom";
 
 
 const DEFAULT_ROUTE = "http://localhost:1522";
@@ -57,19 +58,16 @@ function Profile({sections}) {
         phone: userData.PHONE
       });
 
-       // Guarda los datos traidos
-        setName(userData.FIRST_NAME)
-        setEmail(userData.EMAIL)
-        setLastname(userData.LAST_NAME_1)
-        setRole(userData.USER_TYPE)
-
-        // Cambia el estado de la página
-        setLoading(false);
+      // Guarda los datos traidos
+      setName(userData.FIRST_NAME)
+      setEmail(userData.EMAIL)
+      setLastname(userData.LAST_NAME_1)
+      setRole(userData.USER_TYPE)
     } catch {
       alert('Ocurrió un error al obtener la información de usuario.');
       navigate('/login');
     } finally {
-      () => setLoading(true)
+      setLoading(false);
     }
   };
 
@@ -82,7 +80,6 @@ function Profile({sections}) {
     const sessionUserData = jwtDecode(token);
 
     const apellidos = data.lastname.trim().split(' ');
-
     const apellido1 = apellidos[0] || '';
     const apellido2 = apellidos[1] || '';
 
@@ -117,20 +114,23 @@ function Profile({sections}) {
     }
   };
 
-  if (!loading) {return (
-    <>   
-      <Header name={name} lastname={lastname} role={role} email={email}>
-        {/*Menu de hamburguesa*/}
-        <SideNav id="side-nav-mobile" sections={sections} />
-      </Header>
+  if (loading) return <LoadingPage />;
 
-      <div className="main-container-profile">
-        <div id="side-nav-desktop">
-          <SideNav className="profile-nav" sections={sections} />
+  return (
+    <> 
+      <div className="profile-header-container">
+        <Header name={name} lastname={lastname} role={role} email={email}>
+          {/*Menu de hamburguesa*/}
+          <SideNav id="profile-sidenav-mobile" sections={sections} />
+        </Header>
+      </div>  
+
+      <div className="profile-sidenav-main">
+        <div id="profile-sidenav-desktop">
+          <SideNav className="profile-sidenav" sections={sections} />
         </div>
 
-
-        <div className="main">
+        <div className="profile-main">
           {showSuccess && (
             <AlertMessage
               message={"Información actualizada con éxito"}
@@ -141,20 +141,22 @@ function Profile({sections}) {
             />
           )}
 
-          <div className="profile-container">
+          <div className="profile-title-form-container">
             <h1>Información de perfil</h1>
             <div className="image-form-container">
-              <div className="image-container-profile">
-                <label htmlFor="image-upload-profile" className="upload-label">
-                  <i className="bi bi-pencil edit-icon"></i>
-                  <img src={ProfilePhoto} alt="Foto de perfil editable" className="editable-profile-photo" width='400' height='400'/>
+              <div className="profile-image-container">
+                <label htmlFor="profile-image-upload" className="upload-label">
+                  <div className="edit-icon-container"><i className="bi bi-pencil profile-edit-icon"></i></div>
+                  <img src={ProfilePhoto} alt="Foto de perfil editable"/>
                 </label>
-
-
+                <input id="profile-image-upload" type="file" accept="image/*" style={{ display: "none" }} />
               </div>
+
               <div className="user-info-form">
-                <form onSubmit={(handleSubmit(onSubmit))} className="auth-form">
-                  <label htmlFor="name">Nombre *</label>
+                <form onSubmit={(handleSubmit(onSubmit))} className="auth-form" id="profile-auth-form">
+                  <label htmlFor="name">
+                    Nombre <span style={{ color: 'red' }}>*</span>
+                  </label>
                   <input
                     type="text"
                     placeholder={name}
@@ -162,7 +164,9 @@ function Profile({sections}) {
                   />
                   {errors.name && <span className="error">{errors.name.message}</span>}
 
-                  <label htmlFor="lastname">Apellidos *</label>
+                  <label htmlFor="lastname">
+                    Apellidos <span style={{ color: 'red' }}>*</span>
+                  </label>
                   <input
                     type="text"
                     placeholder=""
@@ -170,7 +174,9 @@ function Profile({sections}) {
                   />
                   {errors.lastname && <span className="error">{errors.lastname.message}</span>}
                   
-                  <label htmlFor="email">Correo electrónico *</label>
+                  <label htmlFor="email">
+                    Correo electrónico <span style={{ color: 'red' }}>*</span>
+                  </label>
                   <input
                     type="email"
                     placeholder=""
@@ -185,7 +191,9 @@ function Profile({sections}) {
                   {errors.email && <span className="error">{errors.email.message}</span>}
 
 
-                  <label htmlFor="phone">Número telefónico *</label>
+                  <label htmlFor="phone">
+                    Número telefónico <span style={{ color: 'red' }}>*</span>
+                  </label>
                   <input
                     type="tel"
                     placeholder=""
@@ -209,7 +217,7 @@ function Profile({sections}) {
         </div>
       </div>
     </>
-  )}
+  )
 }
 
 export default Profile;
