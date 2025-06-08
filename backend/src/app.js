0cbd03873c83d383c20288c262d573d8e371eb14
JORGE.QUIROSANDERSON <JORGE.QUIROSANDERSON@ucr.ac.cr>
2025-06-07 23:02:38 -0600

    feature: admin products full CRUD

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ const emailRoutes = require('./routes/emails');
 const zonesRoutes = require('./routes/zones');
 const dashboardRoutes = require('./routes/dashboard');
 const verifyToken = require('./middleware/verifyToken');
+const productsRoutes = require('./routes/products');
 
 const app = express();
 app.use(cors());
@@ -15,6 +16,7 @@ app.use('/users', usersRoutes);
 app.use('/email', emailRoutes);
 app.use('/zones', zonesRoutes);
 app.use('/dashboard', dashboardRoutes);
+app.use('/products', productsRoutes);
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
